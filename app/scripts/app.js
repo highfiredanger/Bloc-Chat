0a6cd1b920a2af01707d51c20929224e46e4d6ba
Samantha Harms <pearl@Samantha-Lynn-Harmss-MacBook-Pro.local>
2017-01-10 16:00:12 -0700

    First changes

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+(function(){
+    function config($stateProvider){ 
+        
+        $stateProvider
+            .state('home', {
+                url: '/',
+                controller: 'HomeCtrl as home',
+                templateUrl: '/templates/home.html'
+        });
+        
+    }
+    
+    angular
+        .module('blocChat', ['firebase', 'ui.router'])
+        .config(config);
+    
+})();
